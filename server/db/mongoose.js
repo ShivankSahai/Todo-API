7774d9d88628a80cd1a36c6b8a3d768a1c338f60
ShivankSahai <shivank.sahai2017@vitstudent.ac.in>
2018-07-08 02:48:42 +0530

    more

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,6 +1,6 @@
 let mongoose = require('mongoose')
 
 mongoose.Promise = global.Promise
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/TodoApp')
+mongoose.connect('mongodb://ShivankSahai:pizza123!!@ds129821.mlab.com:29821/todoapp' || 'mongodb://localhost:27017/TodoApp')
 
 module.exports={mongoose}
